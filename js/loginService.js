6f2fa1f864c21b8ff6de60da51240e440f46a7cd
OscarIvanBecerra <oibecerrao@ufpso.edu.co>
2025-05-13 18:37:42 -0500

    Clase Mayo6

--- js/loginService.js
+++ js/loginService.js
@@ -1,27 +1,57 @@
-document.getElementById('LoginForm').addEventListener('submit' , function(e){
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    let mensaje = ''
-    let tipoAlerta = ''
-    if(email === '' || password === ''){
-        mensaje = 'Completa los campos'
-        tipoAlerta = 'warning'
-    }
-    else if(email === 'test@test' && password === '123456'){
-        mensaje = 'Login Accesfull'
-        tipoAlerta = 'success'
-    }
-    else{
-        mensaje = 'Error data'
-        tipoAlerta = 'danger'
-    }
-    let alerta  = `
-        <div class="alert alert-${tipoAlerta} alert-dismissible fade show" role="alert">
-        ${mensaje}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        `;
-        document.getElementById('mensaje').innerHTML = alerta;
+document.getElementById('loginForm').addEventListener('submit', function(e){
+	e.preventDefault();
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+	login(email, password)
 });
+
+function login(email, password){
+	localStorage.removeItem('token')
+	let message = ''
+	let alertType = ''
+	
+	const REQRES_ENDPOINT = 'https://reqres.in/api/login'
+	fetch(REQRES_ENDPOINT, {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+			'x-api-key': 'reqres-free-v1'
+		},
+		body: JSON.stringify({email, password})
+	})
+	.then((response) => {
+		if(response.status === 200){
+			alertType = 'success'
+			message = 'inicio de sesion exitoso'
+			alertBuilder(alertType, message)
+			localStorage.setItem('token', 'dbhjbdjsdnasnssg')
+			setTimeout(() => {
+				location.href = 'admin/dashboard.html';
+			}, 2000)
+			
+		}
+		else{
+			alertType = 'danger'
+			message = 'inicio de sesion invalida'
+			alertBuilder(alertType, message)
+		}
+		console.log('respuesta del servicio', response)
+		
+	})
+	.catch((error) =>{
+		alertType = 'danger'
+		message = 'ocurrio un error inesperado'
+		console.log('error en el servicio', error)
+		alertBuilder(alertType, message)
+	})
+	
+}
+
+function alertBuilder(alertType, message){
+	const alert =
+	 `<div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
+		${message}
+		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+	</div>`;
+	document.getElementById('mensaje').innerHTML = alert;
+}
