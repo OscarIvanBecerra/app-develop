0f471eaa45645daf0222854720120520476222cd
OscarIvanBecerra <oibecerrao@ufpso.edu.co>
2025-05-13 19:06:43 -0500

    Arreglos2.0

--- js/loginService.js
+++ js/loginService.js
@@ -1,57 +1,55 @@
-document.getElementById('loginForm').addEventListener('submit', function(e){
-	e.preventDefault();
-	const email = document.getElementById('email').value;
-	const password = document.getElementById('password').value;
-	login(email, password)
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email,password)
 });
 
-function login(email, password){
-	localStorage.removeItem('token')
-	let message = ''
-	let alertType = ''
-	
-	const REQRES_ENDPOINT = 'https://reqres.in/api/login'
-	fetch(REQRES_ENDPOINT, {
-		method: 'POST',
-		headers: {
-			'content-type': 'application/json',
-			'x-api-key': 'reqres-free-v1'
-		},
-		body: JSON.stringify({email, password})
-	})
-	.then((response) => {
-		if(response.status === 200){
-			alertType = 'success'
-			message = 'inicio de sesion exitoso'
-			alertBuilder(alertType, message)
-			localStorage.setItem('token', 'dbhjbdjsdnasnssg')
-			setTimeout(() => {
-				location.href = 'admin/dashboard.html';
-			}, 2000)
-			
-		}
-		else{
-			alertType = 'danger'
-			message = 'inicio de sesion invalida'
-			alertBuilder(alertType, message)
-		}
-		console.log('respuesta del servicio', response)
-		
-	})
-	.catch((error) =>{
-		alertType = 'danger'
-		message = 'ocurrio un error inesperado'
-		console.log('error en el servicio', error)
-		alertBuilder(alertType, message)
-	})
-	
+function login(email, password) {
+    let message= ''
+    let alertType=''
+    
+    const REQRES_ENDPOINT = 'https://reqres.in/api/login'
+    fetch(REQRES_ENDPOINT,  {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'x-api-key':'reqres-free-v1'
+        },
+        body: JSON.stringify({email,password})
+    })
+    .then((response) =>{
+        if (response.status ===200) {
+            alertType ='success'
+            message ='inicio exitoso'
+            alertBuilder(alertType,message)
+            localStorage.setItem('token',"hola")
+            setTimeout (()=>{
+                location.href='admin/dashboard.html'
+            },2000)
+        } else {
+            alertType='danger'
+            message='correo o contraseña invalida'
+        }
+        
+
+        console.log('respuesta del servicio', response)
+        alertBuilder(alertType, message)
+    })
+    .catch((error)=>{
+        alertType ='danger'
+        message = 'error inesperado'
+        console.log('error en el setvicio', error)
+        alertBuilder(alertType, message)
+    })
 }
 
-function alertBuilder(alertType, message){
-	const alert =
-	 `<div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
-		${message}
-		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-	</div>`;
-	document.getElementById('mensaje').innerHTML = alert;
+
+function alertBuilder(alertType, message) {
+    const alert= `<div class="alert alert-${alertType} alert-dismissible fade show" role="alert" >
+    ${message}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`
+    document.getElementById('mensaje').innerHTML=alert;
 }
+
+  
